07cc5369be9e6dd4ddbaa3b33ee32903b875fa89
raports <ramis.khasianov@raports.net>
2024-01-11 23:31:11 +0500

    mobile menu buttons

--- components/MobileMenu.jsx
+++ components/MobileMenu.jsx
@@ -1,7 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import { FaBars } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import { HiOutlineX, HiOutlineMenu } from "react-icons/hi";
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,14 +12,28 @@ const MobileMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  return <div className="tablet:hidden">
-    <button
-        className="text-grey-700 hover:text-blue transition-colors "
-        onClick={toggleMenu}
-      >
-        <FaBars />
+  return (
+    <div className="tablet:hidden">
+      <button className="text-grey-700 transition-colors pt-2" onClick={toggleMenu}>
+        <HiOutlineMenu size={24}/>
       </button>
-  </div>;
+
+      <div
+        className={`w-full h-full bg-white fixed top-0 left-0 z-50 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="flex items-center justify-between h-20 max-width border-b border-grey-100">
+          <Link href="/" className="flex lg:flex-1">
+            <Image src="/logo.svg" alt="logo" width={120} height={40} />
+          </Link>
+          <button className="text-grey-700 " onClick={toggleMenu}>
+            <HiOutlineX size={24} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MobileMenu;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
   const pathname = usePathname();
   const [toggle, setToggle] = useState(false);
   return (
-    <header className="flex items-center justify-between h-20 max-width border-b border-peach">
+    <header className="flex items-center justify-between h-20 max-width border-b border-grey-100">
       {/* Logo */}
       <Link href="/" className="flex lg:flex-1">
         <Image src="/logo.svg" alt="logo" width={120} height={40} />
