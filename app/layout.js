4b2b619b6f007f82ab0401b9860e4be2e324f4f0
raports <ramis.khasianov@raports.net>
2024-01-12 23:43:52 +0500

    footer setup

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,24 @@
-import { Open_Sans } from 'next/font/google'
-import '@/app/ui/globals.css';
-import Navbar from '@/components/Navbar';
+import { Open_Sans } from "next/font/google";
+import "@/app/ui/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Raports - Ramis\'s portfolio',
-  description: 'Ramis Khasianov\'s portfolio website. A place where to show projects and skills.',
-}
+  title: "Raports - Ramis's portfolio",
+  description:
+    "Ramis Khasianov's portfolio website. A place where to show projects and skills.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>
+      <body className={`${openSans.className}`}>
         <Navbar />
-        <div>
-          {children}
-        </div>
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
