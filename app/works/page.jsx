a29d718ff61b07532b873c7f7d2835d72fbbce50
raports <ramis.khasianov@raports.net>
2024-01-22 23:39:25 +0500

    works page

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,3 +1,19 @@
+import React from "react";
+import { works } from "@/constants";
+import WorkCard from "@/components/WorkCard";
+import SectionHeader from "@/components/SectionHeader";
+
 export default function WorksPage() {
-  return <p>Works Page</p>;
+  return (
+    <main>
+      <section className="max-width section-padding">
+        <SectionHeader title="My works" subtitle="Get to know my style" />
+        <div className="grid grid-cols-3 gap-12">
+          {works.map((work, index) => (
+            <WorkCard key={index} {...work} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
